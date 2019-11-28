19dabd4963b4ac30a60465d937ab9bd2cd808af4
Antoine Neveux <antoine.neveux@worldline.com>
2019-11-28 18:33:46 +0100

    :sparkles: implementing a few more steps of the kata

--- src/main/kotlin/com/github/aneveux/kata/rpg/topdown/Game.kt
+++ src/main/kotlin/com/github/aneveux/kata/rpg/topdown/Game.kt
@@ -1,28 +1,54 @@
 package com.github.aneveux.kata.rpg.topdown
 
+import org.jetbrains.annotations.TestOnly
+
 data class Character(val power: Int, val damages: Int, val resistance: Int, val hp: Int) {
     val attackDamage
         get() = power * damages
+
+    val isAlive
+        get() = hp > 0
 
     fun reducedDamage(damage: Int) = if (resistance == 0) damage else damage / resistance
 
     fun receiveDamage(damage: Int) = copy(hp = if (damage >= hp) 0 else hp - damage)
 }
 
-class Battle(val firstPlayer: Character, val secondPlayer: Character) {
+data class Battle(val firstPlayer: Character, val secondPlayer: Character, val rounds: List<Round> = listOf()) {
+    @TestOnly
     fun nextRound() = Round(firstPlayer, secondPlayer)
+
+    fun nextState() = if (rounds.isEmpty()) copy(
+        rounds = listOf(
+            Round(
+                firstPlayer,
+                secondPlayer
+            )
+        )
+    ) else if (rounds.last().isFinal) this else copy(rounds = rounds.plusElement(rounds.last().nextRound()))
 }
 
 data class Round(val firstPlayer: Character, val secondPlayer: Character) {
+    val isFinal
+        get() = !(firstPlayer.isAlive && secondPlayer.isAlive)
+
+    @TestOnly
     fun firstStepResolution() = firstPlayer to with(secondPlayer) {
         receiveDamage(reducedDamage(firstPlayer.attackDamage))
     }
 
+    @TestOnly
     fun secondStepResolution() = with(firstPlayer) {
         receiveDamage(reducedDamage(secondPlayer.attackDamage)) to secondPlayer
     }
 
+    // During refactoring time, I'll simply use some of Kotlin features to have clean and readable code
+    // allowing me to have a simple function describing a character being hit by another one
+    private fun Character.hitBy(opponent: Character) = receiveDamage(reducedDamage(opponent.attackDamage))
+
     // For our baby steps example, I'll reuse the functions I wrote earlier.
     // You'll see that the result isn't the best looking code, so it'll obviously indicate that refactoring is needed!
-    fun resolution() = secondStepResolution().first to firstStepResolution().second
+    fun resolution() = firstPlayer.hitBy(secondPlayer) to secondPlayer.hitBy(firstPlayer)
+
+    fun nextRound() = if (isFinal) this else with(this.resolution()) { Round(first, second) }
 }
--- src/test/kotlin/com/github/aneveux/kata/rpg/topdown/GameTest.kt
+++ src/test/kotlin/com/github/aneveux/kata/rpg/topdown/GameTest.kt
@@ -175,5 +175,113 @@ class GameTest : StringSpec() {
         // Also, at this point I'm getting confident about my source code, so I would do a bit of refactoring.
         // Which I can safely do... because I have tests!
 
+        // For keeping the "history" of the tests and the way I wrote the application, I'll keep the tests and source code
+        // I wrote earlier so you can follow my path.
+        // Another cleaned package will be published in the repository with the actual cleaned up code.
+
+        // Now that the refactoring is done, I'll keep going with the business logic. Basically, once a round is done,
+        // I'd like to know its status. For this, I need to know if Characters are still alive... Let's code that!
+
+        "A character with 0 HPs is considered dead, otherwise it is alive" {
+            val wizard = Character(10, 5, 2, 100)
+            wizard.isAlive shouldBe true
+            wizard.receiveDamage(100).isAlive shouldBe false
+        }
+
+        // We know about our characters, so now we can get some information about the round itself!
+        // A round will be considered final if at least one character dies.
+
+        "A round is final if at least one character dies" {
+            val wizard = Character(10, 5, 2, 0)
+            val paladin = Character(10, 5, 2, 50)
+            val finalRound = Round(wizard, paladin)
+
+            finalRound.isFinal shouldBe true
+
+            val nonFinalRound = Round(paladin, paladin)
+            nonFinalRound.isFinal shouldBe false
+        }
+
+        // Hey! Time to decide what to do if both character die!
+        // In a first place, we'd just say that we consider it as final round as well.
+        "A round is final if both character die" {
+            val paladin = Character(10, 5, 2, 0)
+            val tieRound = Round(paladin, paladin)
+            tieRound.isFinal shouldBe true
+        }
+
+        // We've got enough game logic to start assembling things now!
+        // Basically, what I'd like is to simulate rounds until we find a final state.
+        // The Battle looks like a good place to do so, so let's implement that in there.
+        // First, I'd like to move the nextRound calculation which is right now in Battle
+        // in the Round class, and actually implement it
+
+        "The next round of a final round is itself" {
+            val paladin = Character(10, 5, 2, 0)
+            val finalRound = Round(paladin, paladin)
+            finalRound.nextRound() shouldBe finalRound
+        }
+
+        "The next round of a non final round should be a round starting from the resolution of the current one" {
+            val paladin = Character(10, 2, 1, 100)
+            val round = Round(paladin, paladin)
+            round.nextRound() shouldBe Round(paladin.receiveDamage(20), paladin.receiveDamage(20))
+        }
+
+        // Nice! nextRound doesn't have to be part of the Battle anymore!
+        // Now, the Battle should actually contain all the rounds required for reaching to an end!
+        // Let's add that in the battle!
+
+        "A battle should contain the rounds required for the fight" {
+            val paladin = Character(10, 2, 1, 100)
+            val battle = Battle(paladin, paladin)
+            battle.rounds shouldBe listOf()
+        }
+
+        // Let's continue with baby steps now. The battle should allow to go from state to state, by generating
+        // rounds after rounds.
+        // We'll try continuing with our immutable way of writing code and say that the next state of a battle
+        // is a battle with one more round in it.
+
+        "The next state of a new battle is a battle with one round stored" {
+            val paladin = Character(10, 2, 1, 100)
+            val battle = Battle(paladin, paladin)
+            battle.nextState() shouldBe battle.copy(rounds = listOf(Round(paladin, paladin)))
+        }
+
+        // Next baby step, if there are some already calculated rounds, the next state of a battle should append those
+        // rounds in the list
+
+        "The next state of an ongoing battle is a battle with several rounds stored" {
+            val paladin = Character(10, 2, 1, 100)
+            val battle = Battle(paladin, paladin)
+            battle.nextState() shouldBe battle.copy(rounds = listOf(Round(paladin, paladin)))
+            battle.nextState().nextState() shouldBe battle.copy(
+                rounds = listOf(
+                    Round(paladin, paladin),
+                    Round(paladin.receiveDamage(20), paladin.receiveDamage(20))
+                )
+            )
+        }
+
+        // One more baby step, the next state of a battle which reached a final round is the really same battle
+
+        "The next state of a battle reaching a final round is the same battle" {
+            val paladin = Character(10, 2, 1, 20)
+            val battle = Battle(paladin, paladin)
+            battle.nextState().nextState().nextState() shouldBe battle.copy(
+                rounds = listOf(
+                    Round(paladin, paladin),
+                    Round(paladin.receiveDamage(20), paladin.receiveDamage(20))
+                )
+            )
+        }
+
+        // Fine! We've got ourselves a way to simulate the battle until it ends!
+        // We clearly need some refactoring now. We implemented our baby steps, but we should really merge some things
+        // together!
+        // The tests would probably require some refactoring as well, but we'll do a cleanup afterwards.
+        // We won't modify both the tests and the code at the same time.
+
     }
 }
